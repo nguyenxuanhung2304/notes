98392afbe49b460b5041f97c10ae9f3e7b01bbb3
NxHung <mongtamquoc2015@gmail.com>
2024-03-18 18:11:01 +0900

    fix: add validate_length_of for user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_most(255) }
 
   it { should validate_presence_of(:phone_number) }
+  it { should validate_length_of(:phone_number).is_at_most(255) }
 end
