1224b049fc43511a6b89189a562077b5041730dd
NxHung <mongtamquoc2015@gmail.com>
2024-03-22 13:35:50 +0900

    add: annotate_models for existed models

--- spec/models/directory_spec.rb
+++ spec/models/directory_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: directories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_directories_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Directory do
