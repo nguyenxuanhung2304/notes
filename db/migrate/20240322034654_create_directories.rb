f5ac9294d84c089545d5daf435f82739354a5e8c
NxHung <mongtamquoc2015@gmail.com>
2024-03-22 15:45:43 +0900

    fix: add not null for column user_id

--- db/migrate/20240322034654_create_directories.rb
+++ db/migrate/20240322034654_create_directories.rb
@@ -1,7 +1,7 @@
 class CreateDirectories < ActiveRecord::Migration[7.0]
   def change
     create_table :directories do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, null: false
 
       t.string :name, null: false
 
